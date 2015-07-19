d6435d47221aa4b66fb780e0434119c0fea9d6b2
caotg <caotg@yahoo.com>
2015-07-19 15:33:51 -0700

    sybc

---
+++ data/hpsm_db_test_to_cad_phar_v3_data.sql
@@ -0,0 +1,707 @@
+use CAD_PHAR_V3
+go
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'COB_IMPROPER_COUNT                                                                                  '
+	,N'P'
+	,N'COB Improper                                                                                        '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'COB_OPEN_COUNT                                                                                      '
+	,N'P'
+	,N'COB Open                                                                                            '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'COB_PENDING_COUNT                                                                                   '
+	,N'P'
+	,N'COB Pending                                                                                         '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'COB_PROPER_COUNT                                                                                    '
+	,N'P'
+	,N'COB                                                                                                 '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'COB_RESOLVED_COUNT                                                                                  '
+	,N'P'
+	,N'COB Resolved                                                                                        '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'MEMBER_ID_IMPROPER_COUNT                                                                            '
+	,N'P'
+	,N'Member ID Improper                                                                                  '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'MEMBER_ID_OPEN_COUNT                                                                                '
+	,N'P'
+	,N'Member ID Open                                                                                      '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'MEMBER_ID_PENDING_COUNT                                                                             '
+	,N'P'
+	,N'Member ID Pending                                                                                   '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'MEMBER_ID_PROPER_COUNT                                                                              '
+	,N'P'
+	,N'Member ID                                                                                           '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'MEMBER_ID_RESOLVED_COUNT                                                                            '
+	,N'P'
+	,N'Member ID Resolved                                                                                  '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'MISC_IMPROPER_COUNT                                                                                 '
+	,N'P'
+	,N'Misc Improper                                                                                       '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'MISC_OPEN_COUNT                                                                                     '
+	,N'P'
+	,N'Misc Open                                                                                           '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'MISC_PENDING_COUNT                                                                                  '
+	,N'P'
+	,N'Misc Pending                                                                                        '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'MISC_PROPER_COUNT                                                                                   '
+	,N'P'
+	,N'Misc                                                                                                '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'MISC_RESOLVED_COUNT                                                                                 '
+	,N'P'
+	,N'Misc Resolved                                                                                       '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'PHARMACY_IMPROPER_COUNT                                                                             '
+	,N'P'
+	,N'Pharmacy Improper                                                                                   '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'PHARMACY_OPEN_COUNT                                                                                 '
+	,N'P'
+	,N'Pharmacy Open                                                                                       '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'PHARMACY_PENDING_COUNT                                                                              '
+	,N'P'
+	,N'Pharmacy Pending                                                                                    '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'PHARMACY_PROPER_COUNT                                                                               '
+	,N'P'
+	,N'Pharmacy                                                                                            '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'PHARMACY_RESOLVED_COUNT                                                                             '
+	,N'P'
+	,N'Pharmacy Resolved                                                                                   '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'PRESCRIBER_IMPROPER_COUNT                                                                           '
+	,N'P'
+	,N'Prescriber Improper                                                                                 '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'PRESCRIBER_OPEN_COUNT                                                                               '
+	,N'P'
+	,N'Prescriber Open                                                                                     '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'PRESCRIBER_PENDING_COUNT                                                                            '
+	,N'P'
+	,N'Prescriber Pending                                                                                  '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'PRESCRIBER_PROPER_COUNT                                                                             '
+	,N'P'
+	,N'Prescriber                                                                                          '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'PRESCRIBER_RESOLVED_COUNT                                                                           '
+	,N'P'
+	,N'Prescriber Resolved                                                                                 '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'RX_COST_IMPROPER_COUNT                                                                              '
+	,N'P'
+	,N'Rx Cost Improper                                                                                    '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'RX_COST_OPEN_COUNT                                                                                  '
+	,N'P'
+	,N'Rx Cost Open                                                                                        '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'RX_COST_PENDING_COUNT                                                                               '
+	,N'P'
+	,N'Rx Cost Pending                                                                                     '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'RX_COST_PROPER_COUNT                                                                                '
+	,N'P'
+	,N'Rx Cost                                                                                             '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'RX_COST_RESOLVED_COUNT                                                                              '
+	,N'P'
+	,N'Rx Cost Resolved                                                                                    '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'RX_DURATION_IMPROPER_COUNT                                                                          '
+	,N'P'
+	,N'Rx Duration Improper                                                                                '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'RX_DURATION_OPEN_COUNT                                                                              '
+	,N'P'
+	,N'Rx Duration Open                                                                                    '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'RX_DURATION_PENDING_COUNT                                                                           '
+	,N'P'
+	,N'Rx Duration Pending                                                                                 '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'RX_DURATION_PROPER_COUNT                                                                            '
+	,N'P'
+	,N'Rx Duration                                                                                         '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+INSERT INTO [dbo].[SUP_FIELDS_DEFINITION] (
+	[SOURCE_NAME]
+	,[FIELD_NAME]
+	,[SOURCE_TYPE]
+	,[DISPLAY_NAME]
+	,[DATA_TYPE]
+	,[DATA_CATEGORY]
+	,[CLASS_NAME]
+	)
+VALUES (
+	N'PHAR_FA_REJ_SUMMARY_TABLE                         '
+	,N'RX_DURATION_RESOLVED_COUNT                                                                          '
+	,N'P'
+	,N'Rx Duration Resolved                                                                                '
+	,N'I'
+	,N'R'
+	,NULL
+	)
+
+UPDATE [dbo].[SUP_FIELDS_DEFINITION]
+SET [DATA_TYPE] = N'C'
+WHERE [SOURCE_NAME] = N'PHAR_FA_REJ_POPUP_CLAIM                           '
+	AND [FIELD_NAME] = N'QUANTITY_LIMIT_AMOUNT                                                                               '
+	AND [SOURCE_TYPE] = N'P'
+	AND [DISPLAY_NAME] = N'Qty Lim Amt:                                                                                        '
+	AND [DATA_TYPE] = N'I'
+	AND [DATA_CATEGORY] = N'B'
+	AND [CLASS_NAME] IS NULL
+
+UPDATE [dbo].[SUP_FIELDS_DEFINITION]
+SET [DATA_TYPE] = N'C'
+WHERE [SOURCE_NAME] = N'PHAR_FA_REJ_POPUP_CLAIM                           '
+	AND [FIELD_NAME] = N'QUANTITY_LIMIT_DAYS                                                                                 '
+	AND [SOURCE_TYPE] = N'P'
+	AND [DISPLAY_NAME] = N'Qty Lim Days:                                                                                       '
+	AND [DATA_TYPE] = N'I'
+	AND [DATA_CATEGORY] = N'B'
+	AND [CLASS_NAME] IS NULL
+
+UPDATE [dbo].[SUP_FIELDS_DEFINITION]
+SET [DATA_TYPE] = N'C'
+WHERE [SOURCE_NAME] = N'PHAR_FA_REJ_POPUP_MEMBER                          '
+	AND [FIELD_NAME] = N'QUANTITY_LIMIT_AMOUNT                                                                               '
+	AND [SOURCE_TYPE] = N'P'
+	AND [DISPLAY_NAME] = N'Qty Lim Amt:                                                                                        '
+	AND [DATA_TYPE] = N'I'
+	AND [DATA_CATEGORY] = N'B'
+	AND [CLASS_NAME] IS NULL
+
+UPDATE [dbo].[SUP_FIELDS_DEFINITION]
+SET [DATA_TYPE] = N'C'
+WHERE [SOURCE_NAME] = N'PHAR_FA_REJ_POPUP_MEMBER                          '
+	AND [FIELD_NAME] = N'QUANTITY_LIMIT_DAYS                                                                                 '
+	AND [SOURCE_TYPE] = N'P'
+	AND [DISPLAY_NAME] = N'Qty Lim Days:                                                                                       '
+	AND [DATA_TYPE] = N'I'
+	AND [DATA_CATEGORY] = N'B'
+	AND [CLASS_NAME] IS NULL
+
